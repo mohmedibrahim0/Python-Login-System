8c0e4f1a0a79a0c63f103e0a7d698f56ad57220b
mohmedibrahim0 <mohmed4513@icloud.com>
2026-07-13 23:51:22 +0300

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-
-my_nums = [15, 81, 5, 17, 20, 21, 13]
-my_nums.sort(reverse= True)
-count = 1
-for num in my_nums:
-    if  num % 5 == 0:
-     print(f"{count} => {num}")
-     count +=1
-print("All number painted")
